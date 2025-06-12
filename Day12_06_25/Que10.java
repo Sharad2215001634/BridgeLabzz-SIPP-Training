d9c7148a7e66481c9c0007690f1e2def9e8133b3
Sharad <sharadkumars035@gmail.com>
2025-06-12 13:21:30 +0530

    Kilometer to Miles

---
+++ Day12_06_25/Que10.java
@@ -0,0 +1,15 @@
+package Day12_06_25;
+
+import java.util.Scanner;
+public class Que10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.printf(kilometers+"kilometers is equal to" +  miles + "miles");
+
+        sc.close();
+    }
+}
