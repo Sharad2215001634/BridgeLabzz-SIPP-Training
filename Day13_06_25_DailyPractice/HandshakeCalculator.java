2b154dd431a19aab8caa7569041bd5020120fafe
Sharad <sharadkumars035@gmail.com>
2025-06-13 10:44:30 +0530

    HandSHake

---
+++ Day13_06_25_DailyPractice/HandshakeCalculator.java
@@ -0,0 +1,14 @@
+package Day13_06_25_DailyPractice;
+
+import java.util.Scanner;
+public class HandshakeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = sc.nextInt();
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        System.out.println("Maximum number of handshakes among " + numberOfStudents +
+                           " students is: " + handshakes);
+        sc.close();
+    }
+}
